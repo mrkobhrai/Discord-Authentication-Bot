8adc0f20fb47c05e5503dbc043ba63b59b50cbe2
karan obhrai <mrkobhrai@gmail.com>
2021-10-09 22:21:33 +0100

    Revert "Disable reminders"
    This reverts commit d14ab969f4a7035447a26101f0e2897666907345.

--- index.js
+++ index.js
@@ -617,7 +617,6 @@ async function sync_meetings(){
  * authentication URL
  */
 async function notify_unverified_users(){
-    return;
     var notifications = 0;
     if(configured){
         log("Beginning: Notifiying Unverified Users");
